d5a8d08fad88e8dd26794bb0e789554b788b5c0e
vinay-nmamit <vk5022702@gmail.com>
2024-02-08 19:31:56 +0530

    sort

--- sorting/selectionsort.c
+++ sorting/selectionsort.c
@@ -1,27 +1,32 @@
 // C program for implementation of selection sort 
 #include <stdio.h> 
 
-void swap(int *a, int *b) 
+void swap(int *xp, int *yp) 
 { 
-	int temp = *a; 
-	*a = *b; 
-	*b = temp; 
+	int temp = *xp; 
+	*xp = *yp; 
+	*yp = temp; 
 } 
 
 void selectionSort(int arr[], int n) 
 { 
 	int i, j, k; 
+
+	
 	for (i = 0; i < n-1; i++) 
 	{ 
+		
 		k = i; 
 		for (j = i+1; j < n; j++) 
 		if (arr[j] < arr[k]) 
 			k = j; 
 
+	
 		if(k != i) 
 			swap(&arr[k], &arr[i]); 
 	} 
 } 
+
 
 void printArray(int arr[], int size) 
 { 
@@ -31,27 +36,12 @@ void printArray(int arr[], int size)
 	printf("\n"); 
 } 
 
-int main()
-{
-    int n;
-
-    
-    printf("Enter the size of the array: ");
-    scanf("%d", &n);
-
-    int arr[n];
-
-
-    printf("Enter the elements of the array: ");
-    for (int i = 0; i < n; i++)
-        scanf("%d", &arr[i]);
-
-    selectionSort(arr, n);
-
-    printf("Sorted array: \n");
-
- 
-    printArray(arr, n);
-
-    return 0;
-}
+int main() 
+{ 
+	int arr[] = {64, 25, 12, 22, 11}; 
+	int n = sizeof(arr)/sizeof(arr[0]); 
+	selectionSort(arr, n); 
+	printf("Sorted array: \n"); 
+	printArray(arr, n); 
+	return 0; 
+} 
