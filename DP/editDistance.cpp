c0af36745f5052899c63dfc78fca55ea7202bfde
vinay-nmamit <vk5022702@gmail.com>
2024-04-16 16:57:55 +0530

    edit distance

--- DP/editDistance.cpp
+++ DP/editDistance.cpp
@@ -1,40 +1,42 @@
-#include<iostream>
-#include<vector>
-#include<string>
-using namespace std;
+#include <iostream>
+#include <string>
+#include <vector>
 
-int editDistance(string str1, string str2){
-    int m = str1.size();
-    int n = str2.size();
+int min(int x, int y, int z) {
+    return std::min(std::min(x, y), z);
+}
 
-    int dp[n+1][m+1];
+int edit_distance(const std::string& str1, const std::string& str2) {
+    int m = str1.length();
+    int n = str2.length();
 
-    for(int i = 0; i <= n; i++){
-        dp[i][0] = i;
+    std::vector<std::vector<int>> D(m + 1, std::vector<int>(n + 1));
+
+    for (size_t i{1}; i <= m; ++i) {
+        D.at(i).at(0) = i;
     }
-    for(int j = 0; j <= m; j++){
-        dp[0][j] = j;
+
+    for (size_t j{1}; j <= n; ++j) {
+        D.at(0).at(j) = j;
     }
 
-    for(int i = 1; i <= n; i++){
-        for(int j = 1; j <= m; j++){
-            if(str1[i-1] == str2[j-1]){
-                dp[i][j] = dp[i-1][j-1];
+    for (size_t i{1}; i <= m; ++i) {
+        for (size_t j{1}; j <= n; ++j) {
+            if (str1[i - 1] == str2[j - 1]) {
+                D.at(i).at(j) = D.at(i - 1).at(j - 1);
             } else {
-                dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + 1;
+                D.at(i).at(j) = 1 + min(D.at(i - 1).at(j), D.at(i).at(j - 1), D.at(i - 1).at(j - 1));
             }
         }
     }
-    return dp[n][m];
+
+    return D.at(m).at(n);
 }
 
-int main(){
-    string str1;
-    string str2;
-    getline(cin, str1);
-    getline(cin, str2);
-
-    cout << editDistance(str1, str2) << endl;
-
+int main() {
+    std::string str1;
+    std::string str2;
+    std::cin >> str1 >> str2;
+    std::cout << edit_distance(str1, str2) << std::endl;
     return 0;
 }
