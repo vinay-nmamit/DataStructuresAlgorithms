758ba911128012bc9843c5d520edb8582d38bcbd
vinay-nmamit <vk5022702@gmail.com>
2024-05-16 17:43:23 +0530

    boolean-binary-tree

---
+++ boolBinaryTree.cpp
@@ -0,0 +1,28 @@
+// evaluate boolean binary tree
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr){}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr){}
+    TreeNode(int x,  TreeNode *left, TreeNode *right) : val(x), left(left), right(right){}
+};
+
+bool helper(TreeNode* root){
+    if(root->val  == 0 || root->val == 1){
+        return root->val == 1;
+    } else if(root->val == 2){
+        return helper(root->left) || helper(root->right);
+    } else if(root->val == 3){
+        return helper(root->left) && helper(root->right);
+    }
+
+    return false;
+}
+
+bool evaluateTree(TreeNode* root){
+    return helper(root);
+}
