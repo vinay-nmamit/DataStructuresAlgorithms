53b0d6761e34ec478583994f21982aca4f342fcf
vinay-nmamit <vk5022702@gmail.com>
2024-05-18 10:05:49 +0530

    container with most water

---
+++ containerMostwater.cpp
@@ -0,0 +1,38 @@
+// container with most water
+#include <iostream>
+#include <vector>
+#include <math.h>
+using namespace std;
+
+int Container(vector<int>& height){
+    int left = 0;
+    int right = height.size() - 1;
+    int maxArea = 0;
+
+    while(left < right){
+        int curArea = min(height[left], height[right]) * (right - left);
+        maxArea = max(maxArea, curArea);
+
+        if(height[left] < height[right]){
+            left++;
+        } else {
+            right--;
+        }
+    }
+    return maxArea;
+}
+
+int main(){
+    int size;
+    cout << "enter the size: ";
+    cin >> size;
+    vector<int> height(size);
+    for(int i = 0; i < size; i++){
+        cin >> height[i];
+    }
+
+    int result = Container(height);
+
+    cout << "max amount of water the container can store is: " << result;
+    return 0;
+}
